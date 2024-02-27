0604330ae987883490b2673b83984000d9b7fdd5
Drinnn <pedro_lontro@hotmail.com>
2024-02-26 21:26:43 -0300

    feat: save money

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -1,8 +1,11 @@
+using BayatGames.SaveGameFree;
 using UnityEngine;
 
 public class EconomyManager : MonoBehaviour
 {
     public static EconomyManager Instance;
+
+    private string MONEY_SAVE_KEY = "MONEY";
 
     private int _money;
 
@@ -21,10 +24,16 @@ public class EconomyManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (SaveGame.Exists(MONEY_SAVE_KEY))
+        {
+            _money = SaveGame.Load<int>(MONEY_SAVE_KEY);
+        }
     }
 
     public void AddMoney(int amount)
     {
         _money += amount;
+        SaveGame.Save(MONEY_SAVE_KEY, _money);
     }
 }
